2b94c6c6cbfc568a5edce638b4b6e7cb7b931d4f
Hồ Đăng Nguyện (hdnguyendev) <hdnguyen.dev@gmail.com>
2024-11-18 10:24:25 +0700

    fit something

--- lib/screens/widgets/order_card.dart
+++ lib/screens/widgets/order_card.dart
@@ -1,34 +1,35 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:seafood_app/constants.dart';
 import 'package:seafood_app/model/order_model.dart';
-import 'package:seafood_app/screens/widgets/vip_button.dart';
 
 class OrderCard extends StatelessWidget {
   final OrderModel order;
 
-   OrderCard({super.key, required this.order});
+  OrderCard({super.key, required this.order});
+
   String status = '';
   Color statusColor = Colors.black;
 
   @override
   Widget build(BuildContext context) {
     switch (order.orderStatus) {
-      case 1:
+      case 0:
         status = 'Đang chờ xác nhận';
         statusColor = Colors.orange;
         break;
-      case 2:
+      case 1:
         status = 'Đang giao';
         statusColor = Colors.blue;
         break;
+      case -1:
       case 3:
-        status = 'Hoàn thành';
-        statusColor = Colors.green;
-        break;
-      case 4:
         status = 'Đã huỷ';
         statusColor = Colors.red;
+        break;
+      case 4:
+      case 2:
+        status = 'Hoàn thành';
+        statusColor = Colors.green;
         break;
     }
 
@@ -66,44 +67,44 @@ class OrderCard extends StatelessWidget {
                     ),
                   )),
             ),
-            ListTile(
-              leading: ClipOval(
-                child: Image(
-                  image: Image.network(
-                    order.orderDetails?[0].productImage ?? '',
-                  ).image,
-                  width: 50,
-                  height: 50,
-                  fit: BoxFit.cover,
-                
+            for (var item = 0; item < order.orderDetails!.length; item++)
+              ListTile(
+                leading: ClipOval(
+                  child: Image(
+                    image: Image.network(
+                      order.orderDetails?[item].productImage ?? '',
+                    ).image,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
                 ),
+                title: Text(order.orderDetails?[item].productName ?? 'Hải sản'),
+                subtitle: Text(
+                    'x ${order.orderDetails?[item].productSalesQuantity ?? 0}'),
+                trailing: Text(NumberFormat.currency(
+                  locale: 'vi_VN',
+                  // Set locale to Vietnam
+                  customPattern: '₫###,###,###,###',
+                  // Custom pattern
+                  symbol: '₫',
+                  // Vietnamese currency symbol
+                  decimalDigits: 0, // Set decimal places to 0
+                ).format(double.tryParse(
+                        '${order.orderDetails?[item].productPrice}') ??
+                    0)),
               ),
-              title: Text(order.orderDetails?[0].productName ?? 'Hải sản'),
-              subtitle:
-                  Text('x ${order.orderDetails?[0].productSalesQuantity ?? 0}'),
-              trailing: Text(NumberFormat.currency(
-                locale: 'vi_VN',
-                // Set locale to Vietnam
-                customPattern: '₫###,###,###,###',
-                // Custom pattern
-                symbol: '₫',
-                // Vietnamese currency symbol
-                decimalDigits: 0, // Set decimal places to 0
-              ).format(double.tryParse(
-                  '${order.orderDetails?[0].productPrice}') ??
-                  0)),
-            ),
             Padding(
               padding: const EdgeInsets.only(right: 22, top: 5, bottom: 10),
               child: RichText(
                 text: TextSpan(
-                  style: TextStyle(color: Colors.black),
+                  style: const TextStyle(color: Colors.black),
                   children: [
                     TextSpan(
                         text:
                             'Tổng số tiền (${order.orderDetails?.length} sản phẩm): '),
                     TextSpan(
-                      text:                       NumberFormat.currency(
+                      text: NumberFormat.currency(
                         locale: 'vi_VN',
                         // Set locale to Vietnam
                         customPattern: '₫###,###,###,###',
@@ -111,40 +112,13 @@ class OrderCard extends StatelessWidget {
                         symbol: '₫',
                         // Vietnamese currency symbol
                         decimalDigits: 0, // Set decimal places to 0
-                      ).format(double.tryParse(
-                          '${order.totalPrice}') ??
-                          0),
+                      ).format(double.tryParse('${order.totalPrice}') ?? 0),
                       style: TextStyle(fontWeight: FontWeight.w600),
                     ),
                   ],
                 ),
               ),
             ),
-            // Padding(
-            //   padding: const EdgeInsets.all(8.0),
-            //   child: OverflowBar(
-            //     alignment: MainAxisAlignment.end,
-            //     children: [
-            //       VipButton(
-            //         onPressed: () {},
-            //         icon: Icons.cancel,
-            //         textColor: Colors.white,
-            //         backgroundColor: Colors.redAccent,
-            //         text: 'Hủy Đơn',
-            //         borderRadius: 10,
-            //       ),
-            //       const SizedBox(width: 10),
-            //       VipButton(
-            //         onPressed: () {},
-            //         icon: Icons.forward,
-            //         textColor: Colors.white,
-            //         backgroundColor: Colors.green,
-            //         text: 'Chi Tiết',
-            //         borderRadius: 10,
-            //       ),
-            //     ],
-            //   ),
-            // ),
           ],
         ),
       ),
